6597bd69684fdd1f7950065360ec2847f012a1c1
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-07-18 17:12:00 +0200

    Update

---
+++ app.js
@@ -0,0 +1,9 @@
+const nanodegreeCard = document.querySelector('.card');
+
+//textContent / innerHTML / outerHTML /innerText
+// nanodegreeCard.textContent = 'The <strong>Greatest</strong> Ice Cream Flavors';
+nanodegreeCard.innerHTML = 'The <strong>Greatest</strong> Ice Cream Flavors';
+
+console.log(nanodegreeCard.textContent);
+
+//Add content to the page
