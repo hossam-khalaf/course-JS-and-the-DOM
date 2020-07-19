5c9ce1e9da520006d1f8f6953bef9e5b19d763b9
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-07-19 10:12:19 +0200

    styling with JS

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ nanodegreeCard.innerHTML = 'The <strong>Greatest</strong> Ice Cream Flavors';
 console.log(nanodegreeCard.textContent);
 
 //Add content to the page
+
+const heading = document.querySelector('h1.white');
+const extraText = document.createElement('span');
+extraText.textContent = ' Right Now !';
+
+heading.appendChild(extraText);
+
+// insertAdjacentHTML('beforeend', '')  position and text
+
+//romoving elements  / .removeChild() && .remove()
+
+// properties >> .firstElementChild && .parentElement
+
+// heading.removeChild(extraText);
+
+// Styling
+
+const headingSix = document.querySelector('h6.text-center');
+headingSix.style.color = 'orange';
+headingSix.style.fontSize = '20px';
+headingSix.setAttribute('id', 'orange-heading');
+document.querySelector('#orange-heading').style.backgroundColor = 'cyan';
+nanodegreeCard.style.background = 'tomato';
